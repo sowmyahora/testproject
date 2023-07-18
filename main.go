e5623b6156ec089768c694625030ebf564affbab
Sowmya Hora <sowmyahora01@gmail.com>
2023-07-18 14:56:18 +0530

    Updated main.go

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type address struct {
@@ -25,40 +19,6 @@ type User struct {
 	Address        address  `bson:"Address"`
 	Hobbies        []string `bson:"Hobbies"`
 	ProfilePicture []byte   `bson:"ProfilePicture"`
-}
-
-func insertUser(w http.ResponseWriter, r *http.Request) {
-	var user User
-
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, "Failed to parse request body: "+err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(context.TODO())
-
-	profilepic, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read profile image: "+err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	user.ProfilePicture = profilepic
-
-	collection := client.Database("testdb").Collection("users")
-
-	_, err = collection.InsertOne(context.TODO(), user)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("User created successfully"))
 }
 
 func main() {
